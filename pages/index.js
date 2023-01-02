77382ed44f2163bd857092529ae9860a59ecc910
Mohammed Ihsaanul haque <mohammedihsaanulhaque@Mohammeds-MacBook-Air.local>
2023-01-02 16:20:18 +0530

    initial setup bootstrap and antdesign is added

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>from next js...</p>
+      <button className='btn btn-primary'>bootstrap</button>
+    </>
+  );
+};
+
+export default Index;
